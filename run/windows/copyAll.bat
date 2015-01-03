6a19177df1ed71efc40b0bfa9f20ed97e55e0bf5
krine <carinesantos.cs@gmail.com>
2015-01-03 16:19:36 +0000

    Windows scripts

--- run/windows/copyAll.bat
+++ run/windows/copyAll.bat
@@ -1,4 +1,7 @@
-cp ..\..\VirtualRaidClient\dist\VirtualRaidClient.jar ..\..\bin\
-cp ..\..\VirtualRaidRepo\dist\VirtualRaidRepo.jar ..\..\bin\
-cp ..\..\VirtualRaidServer\dist\VirtualRaidServer.jar ..\..\bin\
-cp ..\..\VirtualRaidLibrary\dist\VirtualRaidLibrary.jar ..\..\bin\
+mkdir ..\..\bin\
+copy ..\..\VirtualRaidClient\dist\VirtualRaidClient.jar ..\..\bin\
+copy ..\..\VirtualRaidRepo\dist\VirtualRaidRepo.jar ..\..\bin\
+copy ..\..\VirtualRaidServer\dist\VirtualRaidServer.jar ..\..\bin\
+copy ..\..\VirtualRaidLibrary\dist\VirtualRaidLibrary.jar ..\..\bin\
+copy ..\..\VirtualRaidServer\src\credenciais.txt ..\..\bin\
+pause
---
+++ run/windows/makeDir.bat
@@ -0,0 +1,8 @@
+mkdir C:\RAID
+mkdir C:\RAID\Repo1
+mkdir C:\RAID\Repo2
+mkdir C:\RAID\Repo3
+mkdir C:\RAID\Repo4
+mkdir C:\RAID\Client1
+mkdir C:\RAID\Client2
+pause
